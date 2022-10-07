a13cbc42a1227392178d319be32ebe5af1511fe5
Igor (FATEC) <igorgustavogonza@gmail.com>
2022-10-07 10:12:20 -0300

    Primeiro commit - Build simples

---
+++ src/main/java/com/breaking/ct/CacaTalentosApplication.java
@@ -0,0 +1,13 @@
+package com.breaking.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaTalentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaTalentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breaking/ct/CacaTalentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breaking.ct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaTalentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
