98c8a5f9ab4dbe476dbea25ea69df8ff427adbd8
Alek <lopees.alex14@gmail.com>
2022-11-02 15:38:54 -0300

    Home e login configuradas e funcinando com pequenos erros

--- src/main/java/com/breaking/ct/security/UserDetailsServiceImpl.java
+++ src/main/java/com/breaking/ct/security/UserDetailsServiceImpl.java
@@ -1,4 +1,4 @@
-package com.breaking.ct.services;
+package com.breaking.ct.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/breaking/ct/security/WebSecurityConfig.java
+++ src/main/java/com/breaking/ct/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.breaking.ct.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,16 +14,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	// @Autowired
+	// UserDetailsServiceImpl userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic()
-			.and()
-			.authorizeHttpRequests()
-			.anyRequest().authenticated()
-			.and()
-			.csrf().disable();
+		http.authorizeRequests()
+			.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+			.logoutSuccessUrl("/login?logout").permitAll().and().csrf().disable();
+
+			// .httpBasic()
+			// .and()
+			// .authorizeHttpRequests()
+			// .anyRequest().authenticated()
+			// .and()
+			// .csrf().disable();
 	}
 	
 	@Override
@@ -32,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("teste")
 			.password(pc().encode("senha"))
 			.roles("ADMIN");
+
+			// auth.userDetailsService(userDetailsService).passwordEncoder(pc());
 	}
 	
 	@Bean
