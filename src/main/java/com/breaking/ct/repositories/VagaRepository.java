a1a5f710ad4230d5222cc94f116070f07478eb82
Gabriel Lopes <gabriel@gabstep.com.br>
2022-11-19 08:59:47 -0300

    mais fixes, AGORA VAI

--- src/main/java/com/breaking/ct/repositories/VagaRepository.java
+++ src/main/java/com/breaking/ct/repositories/VagaRepository.java
@@ -1,5 +1,7 @@
 package com.breaking.ct.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
