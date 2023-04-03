36f91f73c9adadf13918411471c10b598e665542
Alek <lopees.alex14@gmail.com>
2023-04-02 21:45:33 -0300

    Contador de Caracteres e organização de scripts

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -23,3 +23,29 @@
     scriptExtra.src = 'https://hiperesp.github.io/HiperDialog/src/js/extra.js';
     document.head.appendChild(scriptExtra);
 })();
+
+(function() {
+    function counter(input) {
+        var counterElement = document.createElement("span");
+        counterElement.classList.add("text-muted");
+        counterElement.classList.add("small");
+        counterElement.classList.add("float-end");
+        counterElement.style.pointerEvents = "none";
+        counterElement.update = function(input) {
+            if(input.selectizeSource) {
+                input = input.selectizeSource.$input.get()[0];
+            }
+            this.textContent = (input.value.length).toLocaleString()+" / "+(input.maxLength).toLocaleString();
+        }
+        input.addEventListener("keyup", counterElement.update.bind(counterElement, input));
+        counterElement.update.bind(counterElement, input)();
+        input.parentNode.insertBefore(counterElement, input.nextSibling);
+    }
+    // CONTADOR DE CHARS NAS TEXTAREAS E NOS INPUTS
+    window.addEventListener("load", function() {
+        var inputs = document.querySelectorAll("textarea[maxlength],input[maxlength]:not(.selectized)");
+        for(var i=0; i<inputs.length; i++) {
+            counter(inputs[i]);
+        }
+    });
+})();
