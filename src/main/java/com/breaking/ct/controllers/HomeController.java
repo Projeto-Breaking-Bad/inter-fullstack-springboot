278208561866939baa480126c844ceafe0cc76f7
--global <--global>
2023-05-11 10:50:02 -0300

    Edit home

--- src/main/java/com/breaking/ct/controllers/HomeController.java
+++ src/main/java/com/breaking/ct/controllers/HomeController.java
@@ -1,22 +1,33 @@
 package com.breaking.ct.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.breaking.ct.models.Vaga;
+import com.breaking.ct.services.VagaService;
 
 @RestController
 public class HomeController {
 
+	@Autowired
+	private VagaService vagaService;
+
 	@GetMapping("/")
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("user/home");
-		
+		ArrayList<Vaga> vagas = vagaService.getTodasVagas(); 
+		mv.addObject("vagas", vagas); 
+
 		try { mv.setViewName(tryRedirection()); }
 		catch (Exception e) {}
 		
@@ -31,6 +42,22 @@ public class HomeController {
 		catch (Exception e) {}
 		
 		return mv;
+	}
+
+	@GetMapping("/vagas/consultar/{id}")
+	public ModelAndView verVagasEspecifica(@PathVariable String id) {
+		
+		ModelAndView mv = new ModelAndView("vagaNaoEncontrada");
+		
+		Optional<Vaga> vagaConsultada = vagaService.getVagaById(id);
+		
+		if(vagaConsultada.isEmpty())
+			return mv;
+		
+		mv.addObject("vaga", vagaConsultada.get());
+		mv.setViewName("user/perfilVaga");
+		return mv;
+		
 	}
 
 	@GetMapping("/acessibilidade")
