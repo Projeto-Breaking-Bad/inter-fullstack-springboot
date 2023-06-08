f16a9efeea71a4a36e3b3db6fced9498b84a3d8f
Alek <lopees.alex14@gmail.com>
2023-06-08 18:21:30 -0300

    Correções no projeto e adição de melhorias no layout, styles, scripts e icons #Status - Em Desenvolvimento

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,4 +1,3 @@
-/** HiperDialog */
 (function() {
     var linkColor = document.createElement('link');
     linkColor.rel = 'stylesheet';
@@ -46,6 +45,84 @@
         var inputs = document.querySelectorAll("textarea[maxlength],input[maxlength]:not(.selectized)");
         for(var i=0; i<inputs.length; i++) {
             counter(inputs[i]);
+        }
+    });
+})();
+
+$(document).ready(function(){
+    var fonte = 17;
+    
+    (function() {
+        $('#aumentar').on('click', function(){
+            if(fonte < 23){
+                fonte = fonte + 1;
+                $('body p, body span').css('font-size', fonte+'px');
+            }
+        });
+        
+        $('#diminuir').on('click', function(){
+            if(fonte > 17){
+                fonte = fonte - 1;
+                $('body p, body span').css('font-size', fonte+'px');
+            }
+        });
+    })();
+});
+
+(function () {
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#logradouro").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        // $("#ibge").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#logradouro").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                // $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (
+                    dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#logradouro").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        // $("#ibge").val(dados.ibge);
+                    } else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alertDialog("CEP não encontrado.");
+                    }
+                });
+            } else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alertDialog("Formato de CEP inválido.");
+            }
+        } else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
         }
     });
 })();
