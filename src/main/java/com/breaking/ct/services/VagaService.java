c2f3b091c6ec6cc9beb68b265df38556156e6004
Igor <igor.gonzalez@fatec.sp.gov.br>
2023-06-11 20:37:53 -0300

    Sistema de inscrição de aluno em vaga

--- src/main/java/com/breaking/ct/services/VagaService.java
+++ src/main/java/com/breaking/ct/services/VagaService.java
@@ -7,25 +7,36 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.breaking.ct.models.Aluno;
 import com.breaking.ct.models.Vaga;
 import com.breaking.ct.repositories.VagaRepository;
 
 @Service
 public class VagaService {
-    @Autowired
-	private VagaRepository vagaRepository;
+
+	@Autowired
+	private AlunoService alunoService;
 	
+	@Autowired
+	private VagaRepository vagaRepository;
+
 	public ArrayList<Vaga> getTodasVagas() {
 		ArrayList<Vaga> vagas = new ArrayList<>();
 		vagaRepository.findAll().forEach(vagas::add);
 		return vagas;
 	}
 
-	public List<Vaga> getVagasByCnpj(String cnpj){
+	public List<Vaga> getVagasByListaIds(List<String> listaIds) {
+		ArrayList<Vaga> vagas = new ArrayList<>();
+		vagaRepository.findAllById(listaIds).forEach(vagas::add);
+		return vagas;
+	}
+
+	public List<Vaga> getVagasByCnpj(String cnpj) {
 		return vagaRepository.findByCnpj(cnpj);
 	}
 
-	public Optional<Vaga> getVagaById(String id){
+	public Optional<Vaga> getVagaById(String id) {
 		return vagaRepository.findById(id);
 	}
 
@@ -40,5 +51,53 @@ public class VagaService {
 	public void deleteVaga(String id) {
 		vagaRepository.deleteById(id);
 	}
-	
+
+	public void inscreverVaga(String id) {
+		
+		Aluno alunoLogado = alunoService.getLogged();
+
+		Optional<Vaga> consultaVaga = vagaRepository.findById(id);
+		if (consultaVaga.isEmpty())
+			return;
+
+		Vaga vagaParaAplicar = consultaVaga.get();
+		if (alunoLogado.getListaIdVagasAplicadas().contains(vagaParaAplicar.getId()))
+			return;
+
+		List<String> listaIdVagasNova = alunoLogado.getListaIdVagasAplicadas();
+		listaIdVagasNova.add(vagaParaAplicar.getId());
+		alunoLogado.setListaIdVagasAplicadas(listaIdVagasNova);
+		alunoService.updateAluno(alunoLogado);
+
+		List<String> listaIdAlunosNova = vagaParaAplicar.getListaIdsAlunosAplicantes();
+		listaIdAlunosNova.add(alunoLogado.getCpf());
+		alunoLogado.setListaIdVagasAplicadas(listaIdAlunosNova);
+
+		vagaRepository.save(vagaParaAplicar);
+	}
+
+	public void desinscreverVaga(String id) {
+
+		Aluno alunoLogado = alunoService.getLogged();
+
+		Optional<Vaga> consultaVaga = vagaRepository.findById(id);
+		if (consultaVaga.isEmpty())
+			return;
+
+		Vaga vagaParaAplicar = consultaVaga.get();
+		if (!alunoLogado.getListaIdVagasAplicadas().contains(vagaParaAplicar.getId()))
+			return;
+
+		List<String> listaIdVagasNova = alunoLogado.getListaIdVagasAplicadas();
+		listaIdVagasNova.remove(vagaParaAplicar.getId());
+		alunoLogado.setListaIdVagasAplicadas(listaIdVagasNova);
+		alunoService.updateAluno(alunoLogado);
+
+		List<String> listaIdAlunosNova = vagaParaAplicar.getListaIdsAlunosAplicantes();
+		listaIdAlunosNova.remove(alunoLogado.getCpf());
+		alunoLogado.setListaIdVagasAplicadas(listaIdAlunosNova);
+
+		vagaRepository.save(vagaParaAplicar);
+	}
+
 }
